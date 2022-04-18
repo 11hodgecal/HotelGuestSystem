f677cf18144dddfee988da5a698a6c696388595e
SPP User <spp-user@spp-forum.de>
2022-04-18 18:41:13 +0100

    Added the Currency converter object

---
+++ HotelGuestSystem/Models/GetRates.cs
@@ -0,0 +1,29 @@
+﻿using HotelGuestSystem.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelGuestSystem.Models
+{
+    public static class GetRates
+    {
+        //converts pounds to dollars
+        public static double ToUSD(ApplicationDbContext db, double GBP)
+        {
+            var rate = db.GBPConversionRates.FirstOrDefaultAsync().Result.GDPToUSDRate;
+            return GBP * rate;
+        }
+        //converts pounds to euros
+        public static double ToEuro(ApplicationDbContext db, double GBP)
+        {
+            var rate = db.GBPConversionRates.FirstOrDefaultAsync().Result.GDPToEuroRate;
+            return GBP * rate;
+        }
+        //converts pounds to yen
+        public static double ToYen(ApplicationDbContext db, double GBP)
+        {
+            var rate = db.GBPConversionRates.FirstOrDefaultAsync().Result.GDPToYenRate;
+            return GBP * rate;
+        }
+    }
+}
