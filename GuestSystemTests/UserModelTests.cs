6b4b559930843956245246dc3ac3259cf287bc49
11Hodgecal <callumhodge456@protonmail.com>
2022-04-20 21:37:41 +0100

    Added a Usermodel test

---
+++ GuestSystemTests/UserModelTests.cs
@@ -0,0 +1,27 @@
+﻿using HotelGuestSystem.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuestSystemTests
+{
+    public class UserModelTests
+    {
+        [Test]
+        public async Task FullnameStringReturnsCorrectly()
+        {
+            //Arrange
+            UserModel test = new UserModel();
+            test.Fname = "test";
+            test.Sname = "test";
+            var expected = "test test";
+            //Act
+            var actual = test.Fullname();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
