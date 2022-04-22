da6f2934c5285a15ec948a9f27bc45a7a6171d94
11Hodgecal <callumhodge456@protonmail.com>
2022-04-22 19:10:33 +0100

    Added service view tests

--- GuestSystemTests/ConverterTests.cs
+++ GuestSystemTests/ConverterTests.cs
@@ -18,10 +18,19 @@ namespace GuestSystemTests
             var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString()).Options;
             _db = new ApplicationDbContext(options);
             var newrates = new CurrentRatesModel();
+
             newrates.GDPToUSDRate = 1.1;
             newrates.GDPToEuroRate = 1.2;
             newrates.GDPToYenRate = 1.3;
             _db.GBPConversionRates.Add(newrates);
+
+            var newservice = new RoomServiceModel()
+            {
+                Id = 1,
+                Name = "test",
+                Price = 1,
+            };
+            _db.RoomServiceItems.Add(newservice);
             await _db.SaveChangesAsync();
             _db.Database.EnsureCreated();
         }
@@ -69,7 +78,67 @@ namespace GuestSystemTests
             //assert
             Assert.AreEqual(expected, actual, "The Euro Converstion does not work");
         }
+        [Test]
+        public async Task ToPoundServiceViewWorks()
+        {
+            //arrange
+            await CreateMocDBAsync();
+            var RoomServiceItems = await _db.RoomServiceItems.ToListAsync();
+            string Currency = "GBP";
+            string expected = "£1";
+            //Act
+            var Actual = GetRates.ServiceRequestViewModelConvert(_db, RoomServiceItems, Currency)[0].PreferedCurPrice;
 
-        
+            //Assert
+            Assert.AreEqual(expected, Actual);
+
+        }
+        [Test]
+        public async Task ToUSDServiceViewWorks()
+        {
+            //arrange
+            await CreateMocDBAsync();
+            var RoomServiceItems = await _db.RoomServiceItems.ToListAsync();
+            string Currency = "USD";
+            string expected = "$1.1";
+            //Act
+            var Actual = GetRates.ServiceRequestViewModelConvert(_db, RoomServiceItems, Currency)[0].PreferedCurPrice;
+
+            //Assert
+            Assert.AreEqual(expected, Actual);
+
+        }
+        [Test]
+        public async Task ToEuroServiceViewWorks()
+        {
+            //arrange
+            await CreateMocDBAsync();
+            var RoomServiceItems = await _db.RoomServiceItems.ToListAsync();
+            string Currency = "EURO";
+            string expected = "€1.2";
+            //Act
+            var Actual = GetRates.ServiceRequestViewModelConvert(_db, RoomServiceItems, Currency)[0].PreferedCurPrice;
+
+            //Assert
+            Assert.AreEqual(expected, Actual);
+
+        }
+        [Test]
+        public async Task ToYenServiceViewWorks()
+        {
+            //arrange
+            await CreateMocDBAsync();
+            var RoomServiceItems = await _db.RoomServiceItems.ToListAsync();
+            string Currency = "Yen";
+            string expected = "¥1";
+            //Act
+            var Actual = GetRates.ServiceRequestViewModelConvert(_db, RoomServiceItems, Currency)[0].PreferedCurPrice;
+
+            //Assert
+            Assert.AreEqual(expected, Actual);
+
+        }
+
+
     }
 }
